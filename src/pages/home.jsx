5ab5c82aecbed97bda89f6956b66a8dc336b5eeb
is-potions <albertgagalac@gmail.com>
2024-09-14 02:48:00 +0800

    fix: fixed imports and files used to display page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "../App.css";
-import Navbar from "../components/navbar";
-import AboutUs from "../components/aboutus";
-import ProductList from "../components/products/products";
+import Navbar from "../components/navbar.jsx";
+import AboutUs from "../components/aboutus.jsx";
+import ProductList from "../components/products/products.jsx";
 
 export default function Home() {
     return (
